1e9771ddba858d6e8e3d9bbba15c3c8ab61fd589
Arun Dhungana <a.d.since03@gmail.com>
2024-02-03 17:21:26 +0545

    list all posted job for comapny

--- frontend/src/pages/Job/manage.jsx
+++ frontend/src/pages/Job/manage.jsx
@@ -1,49 +1,81 @@
 import { Col, Row, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
-export const Manage = () => {
-  return (
-    <Col xs={12} className="">
-      <Row>
-        <Col xs={12} md={7} className="mx-auto  mt-3">
-          <Card
-            style={{
-              background:
-                " linear-gradient(to right, #00ff00, #33cc33, #66cc66, #99cc99, #b2d8b2, #ccffcc)",
-            }}
-          >
-            <Card.Body>
-              <Card.Title>Job Title</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">
-                Job Category
-              </Card.Subtitle>
-              <Card.Text>Description of Job</Card.Text>
+import { useEffect, useState } from "react";
+import { Loading } from "../../components/Loading";
+import { useParams } from "react-router-dom";
+import http from "../../http";
 
-              <Row>
-                <Col xs={12} md={6}>
-                  <Card.Link as={Link} to="/job/edit/123" className="">
-                    Edit
-                  </Card.Link>
-                </Col>
-                <Col
-                  xs={12}
-                  md={6}
-                  className="text-md-center "
-                  style={{
-                    textWrap: "nowrap",
-                    overflow: "hidden",
-                    textOverflow: "ellipsis",
-                  }}
-                >
-                  <Card.Link as={Link} to="/application/list/123">
-                    See the appilcants
-                    <i className="fa-solid fa-angles-right"></i>
-                  </Card.Link>
-                </Col>
-              </Row>
-            </Card.Body>
-          </Card>
+export const Manage = () => {
+  const [loading, setLoading] = useState(false);
+
+  const [job, setJob] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    http
+      .get(`/profile/jobs`)
+      .then(({ data }) => setJob(data))
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+  return loading ? (
+    <Loading></Loading>
+  ) : job.length ? (
+    job.map((jobs) => {
+      return (
+        <Col xs={12} className="">
+          <Row>
+            <Col xs={12} md={7} className="mx-auto  my-3">
+              <Card
+                style={{
+                  background:
+                    " linear-gradient(to right, #00ff00, #33cc33, #66cc66, #99cc99, #b2d8b2, #ccffcc)",
+                }}
+              >
+                <Card.Body>
+                  <Card.Title>{jobs.title}</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    {jobs.category}
+                  </Card.Subtitle>
+                  <Card.Text>{jobs.description}</Card.Text>
+
+                  <Row>
+                    <Col xs={12} md={6}>
+                      <Card.Link
+                        as={Link}
+                        to={`/job/edit/${jobs.job_id}`}
+                        className=""
+                      >
+                        Edit
+                      </Card.Link>
+                    </Col>
+                    <Col
+                      xs={12}
+                      md={6}
+                      className="text-md-center "
+                      style={{
+                        textWrap: "nowrap",
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                      }}
+                    >
+                      <Card.Link
+                        as={Link}
+                        to={`/application/list/${jobs.job_id}`}
+                      >
+                        See the appilcants
+                        <i className="fa-solid fa-angles-right"></i>
+                      </Card.Link>
+                    </Col>
+                  </Row>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
         </Col>
-      </Row>
-    </Col>
-  );
+      );
+    })
+  ) : null;
 };
