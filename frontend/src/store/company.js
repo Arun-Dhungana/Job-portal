aa1f65f6a9f8dc8663b9f7ebd9998b9655d5f0dd
Arun Dhungana <a.d.since03@gmail.com>
2024-02-04 20:06:06 +0545

    created company slice

---
+++ frontend/src/store/company.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+const company = createSlice({
+  name: "company",
+  initialState: { value: {} },
+  reducers: {
+    setCompany: (state, action) => {
+      state.value = action.payload;
+      console.log(state.value);
+    },
+    clearCompany: (state, action) => {
+      state.value = {};
+    },
+  },
+});
+export const { setCompany, clearCompany } = company.actions;
+export default company.reducer;
