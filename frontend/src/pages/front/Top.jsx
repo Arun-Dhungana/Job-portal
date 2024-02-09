429fbc1be0c739d53db8bd11fa4164252c20d08e
Arun Dhungana <a.d.since03@gmail.com>
2024-02-09 22:06:02 +0545

    sort,filter,direction

--- frontend/src/pages/front/Top.jsx
+++ frontend/src/pages/front/Top.jsx
@@ -1,29 +1,260 @@
 import React, { useEffect, useState } from "react";
-import { Row, Container } from "react-bootstrap";
-import { JobList } from "../../components/JobList";
+import { Col, Row, Container, Dropdown, Pagination } from "react-bootstrap";
+import { JobCard } from "../../components/JobCard";
 import { Loading } from "../../components/Loading";
 import http from "../../http";
+import topp from "../../lib/top.jpeg";
+import moment from "moment";
 export const Top = () => {
   const [top, setTop] = useState([]);
-  const [loading, setLoading] = useState(false);
+
+  const [sortBy, setSortBy] = useState("");
+  const [direction, setDirection] = useState("asc");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [offset, setOffset] = useState(0);
+  const [perPage, setPerPage] = useState(30);
+  const [totalPages, setTotalPages] = useState(1);
+  const [paginated, setPaginated] = useState([]);
+  const [pageLink, setPageLink] = useState([]);
+  const [category, setCategory] = useState("");
+  const [job, setJob] = useState(top);
   useEffect(() => {
-    setLoading(true);
-    http
-      .get("/front/top")
-      .then(({ data }) => setTop(data))
-      .catch((err) => console.log(err))
-      .finally(() => setLoading(false));
+    const fetch = async () => {
+      try {
+        const response = await http.get("/front/top");
+        setTop(response.data);
+        setJob(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetch();
   }, []);
-  return loading ? (
-    <Loading></Loading>
-  ) : (
+
+  useEffect(() => {
+    if (sortBy.length) {
+      let sorted = [...top].sort((a, b) => {
+        if (isNaN(parseFloat(a[sortBy])) || isNaN(parseFloat(b[sortBy]))) {
+          if (moment(a[sortBy]).isValid() && moment(b[sortBy]).isValid()) {
+            return moment(a[sortBy]) - moment(b[sortBy]);
+          } else {
+            let x = a[sortBy].toLowerCase();
+            let y = b[sortBy].toLowerCase();
+            if (x < y) {
+              return -1;
+            }
+            if (x > y) {
+              return 1;
+            }
+            return 0;
+          }
+        } else {
+          return a[sortBy] - b[sortBy];
+        }
+      });
+      if (direction == "desc") {
+        sorted.reverse();
+      }
+      if (category.length) {
+        if (category != "all") {
+          const sort = sorted.filter((data) => {
+            return data.category == category;
+          });
+          setJob(sort);
+          setCurrentPage(1);
+        }
+      } else {
+        setJob(sorted);
+        setCurrentPage(1);
+      }
+    }
+  }, [sortBy, direction, category]);
+
+  useEffect(() => {
+    let temp = (currentPage - 1) * perPage;
+    setOffset(temp);
+  }, [currentPage]);
+
+  useEffect(() => {
+    setCurrentPage(1);
+    let temp = [...job].splice(offset, perPage);
+    let total = Math.ceil(job.length / perPage);
+
+    setPaginated(temp);
+    setTotalPages(total);
+  }, [perPage, job, offset]);
+
+  useEffect(() => {
+    let temp = [
+      <Pagination.Prev
+        disabled={currentPage == 1}
+        onClick={() => setCurrentPage(currentPage - 1)}
+      ></Pagination.Prev>,
+    ];
+    for (let i = 1; i <= totalPages; i++) {
+      temp.push(
+        <Pagination.Item
+          key={i}
+          active={i == currentPage}
+          onClick={() => {
+            setCurrentPage(i);
+          }}
+        >
+          {i}
+        </Pagination.Item>
+      );
+    }
+    temp.push(
+      <Pagination.Next
+        disabled={currentPage == totalPages}
+        onClick={() => setCurrentPage(currentPage + 1)}
+      ></Pagination.Next>
+    );
+    setPageLink(temp);
+  }, [totalPages, currentPage]);
+
+  const handleSort = (key) => {
+    if (direction == "desc") {
+      setDirection("desc");
+    } else if (direction == "asc") {
+      setDirection("asc");
+    }
+    setSortBy(key);
+  };
+
+  return (
     <Container>
       <Row>
-        <JobList
-          title="top"
-          data={top}
-          sortable={["Name", "Salary", "Opening", "Deadline"]}
-        ></JobList>
+        <Col xs={12} className="mt-3 mb-sm-3 mb-md-5 mx-0 p-0 " style={{}}>
+          <Row className="mx-2">
+            <Col
+              xs={12}
+              className=" text-nowrap ms-0 mb-sm-3 mb-md-5 ps-0"
+              style={{}}
+            >
+              <h1
+                className="  text-capitalize text-center "
+                style={{
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                  fontWeight: "bolder",
+                  backgroundImage: `url(${topp})`,
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  color: "transparent",
+                  backgroundClip: "text",
+                }}
+              >
+                Top Jobs
+              </h1>
+            </Col>
+            <Col xs={6} md={5} className="d-flex flex-row justify-content-end">
+              <h6 className="d-inline p-1">Sort By </h6>
+              <Dropdown className="d-inline mb-1">
+                <Dropdown.Toggle
+                  size="sm"
+                  className=" border border-none text-black d-flex flex-row align-items-center"
+                  style={{
+                    background: "lightgrey",
+                  }}
+                >
+                  {sortBy}
+                </Dropdown.Toggle>
+                <Dropdown.Menu variant="dark">
+                  {["name", "opening", "deadline", "salary"].map(
+                    (data, index) => {
+                      return (
+                        <Dropdown.Item
+                          key={index}
+                          onClick={() => handleSort(data)}
+                          className="text-capitalize"
+                        >
+                          {data}
+                        </Dropdown.Item>
+                      );
+                    }
+                  )}
+                </Dropdown.Menu>
+              </Dropdown>
+            </Col>
+            <Col xs={6} md={5} className="d-flex flex-row justify-content-end">
+              <h6 className="d-inline p-1">Category</h6>
+              <Dropdown className="d-inline mb-1">
+                <Dropdown.Toggle
+                  size="sm"
+                  className=" border border-none text-black"
+                  style={{ background: "lightgrey" }}
+                >
+                  {category}
+                </Dropdown.Toggle>
+                <Dropdown.Menu variant="dark">
+                  {[
+                    "bank-finance",
+                    "ngo-ingo",
+                    "sales-marketing",
+                    "government",
+                    "army-police",
+                    "cooperative",
+                    "school-college",
+                    "healthcare",
+                    "hotel-restaurant",
+                    "customer_care",
+                    "it-computer",
+                    "logistics-supply_chain",
+                    "all",
+                  ].map((data, index) => {
+                    return (
+                      <Dropdown.Item
+                        key={index}
+                        onClick={() => setCategory(data)}
+                        className="text-capitalize"
+                      >
+                        {data}
+                      </Dropdown.Item>
+                    );
+                  })}
+                </Dropdown.Menu>
+              </Dropdown>
+            </Col>
+            <Col xs={12} md={1} className="d-flex flex-row justify-content-end">
+              <i
+                onClick={() => setDirection("asc")}
+                className={`fa-solid fa-arrow-up-short-wide p-2 ${
+                  direction == "asc" ? "text-primary" : null
+                }`}
+              ></i>
+              <i
+                onClick={() => setDirection("desc")}
+                className={`fa-solid fa-arrow-down-short-wide p-2 ${
+                  direction == "desc" ? "text-primary" : null
+                }`}
+              ></i>
+            </Col>
+          </Row>
+          {job.length ? (
+            paginated.length ? (
+              <Container>
+                <Row className="justify-content-evenly ps-2 pe-2 mt-sm-2 mt-md-5">
+                  <JobCard data={paginated}></JobCard>
+                </Row>
+                <Row>
+                  <Col className="d-flex flex-row justify-content-center">
+                    {totalPages > 1 ? pageLink.map((dataa) => dataa) : null}
+                  </Col>
+                </Row>
+              </Container>
+            ) : null
+          ) : (
+            <Row>
+              <Col>
+                <h2 className="text-center text-secondary">
+                  <i className="fa-solid fa-bug me-1"></i>Data Not Found
+                </h2>
+              </Col>
+            </Row>
+          )}
+        </Col>
       </Row>
     </Container>
   );
