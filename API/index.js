2e36e591591de1d27fbbc5b10d5f12a95bf0377e
Arun Dhungana <a.d.since03@gmail.com>
2024-02-12 14:25:40 +0545

    .

--- API/index.js
+++ API/index.js
@@ -5,23 +5,11 @@ const routes = require("./routes/index");
 const cors = require("cors");
 config();
 const app = express();
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://jobhub-nu.vercel.app");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-
-  res.setHeader("Access-Control-Max-Age", 7200);
-
-  next();
-});
+app.use(cors({
+  origin: '*', // Allow requests from any origin
+  methods: 'GET,POST,PUT,DELETE,PATCH', // Allow specified HTTP methods
+  allowedHeaders: 'Content-Type,Authorization,XMLHttpRequest', // Allow specified headers
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
