735fdc41a76875c7967906b96fcd9e6bf9ecb44a
Arun Dhungana <a.d.since03@gmail.com>
2024-02-12 20:22:00 +0545

    ..

--- API/middlewares/index.js
+++ API/middlewares/index.js
@@ -55,7 +55,7 @@ const Auth = async (req, res, next) => {
 const fileStorage = (mimeTypes = []) =>
   multer({
     storage: multer.diskStorage({
-      destination: (req, file, cb) => cb(null, "/uploads"),
+      destination: (req, file, cb) => cb(null, "../uploads"),
       filename: (req, file, cb) => {
         const ext = file.originalname.split(".").pop();
 
